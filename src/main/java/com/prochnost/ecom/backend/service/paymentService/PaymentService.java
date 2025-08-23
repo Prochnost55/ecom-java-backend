7b23e169cea4001b08ee3dad6fa52088c0692f6f
Abhishek Singh <aks3555@gmail.com>
2025-08-23 21:33:15 +0530

    removed payment service from core logic

--- src/main/java/com/prochnost/ecom/backend/service/paymentService/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.prochnost.ecom.backend.service.paymentService;
-
-import com.prochnost.ecom.backend.dto.paymentDto.PaymentRequestDTO;
-import com.prochnost.ecom.backend.dto.paymentDto.PaymentResponseDTO;
-import com.stripe.exception.StripeException;
-
-public interface PaymentService {
-    PaymentResponseDTO makePayment(PaymentRequestDTO paymentRequestDTO) throws StripeException;
-}
